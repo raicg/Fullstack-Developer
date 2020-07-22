9e28ddaf28c226094016d7da2d4a381ea02b6164
Raí Gondim <raicg2@gmail.com>
2020-07-22 03:35:43 -0300

    updating user requests specs

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "/user/profile", type: :request do
 
+  let!(:user) { create(:user) }
+  before { sign_in(user) }
+
   describe "GET /profile" do
     it "returns http success" do
       get user_profile_url
